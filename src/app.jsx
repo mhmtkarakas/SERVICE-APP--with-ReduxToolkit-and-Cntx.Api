f78c7273e709d9a309570b7754701c2bddfc57e2
mhmtkarakas <m.karakas32@gmail.com>
2023-04-18 13:04:57 +0300

    required packages and folders created

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
-
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/footer";
+import Header from "./components/header";
+import CategoryDetailsPage from "./pages/category-details-page";
+import MainPage from "./pages/main-page";
 
 function App() {
-  
-
   return (
-    <div className='container' >
-    burasi ap.jsx
-    </div>
-  )
+    <BrowserRouter>
+      <div className="container py-3">
+        <Header />
+        <main>
+          <Routes>
+            <Route index element={<MainPage />} />
+            <Route path="category">
+              <Route path=":slug" element={<CategoryDetailsPage />} />
+            </Route>
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/category-details-page/index.jsx
@@ -0,0 +1,7 @@
+
+export default function CategoryDetailsPage(){
+    return 
+    <>
+    Burasi kategori detay sayfasi
+    </>
+}
