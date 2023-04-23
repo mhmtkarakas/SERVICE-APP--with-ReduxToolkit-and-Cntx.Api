7371951555361b0978f12819bcce77022d751b2c
mhmtkarakas <m.karakas32@gmail.com>
2023-04-23 12:10:53 +0300

    category details taken

--- src/pages/category-details-page/index.jsx
+++ src/pages/category-details-page/index.jsx
@@ -1,8 +1,41 @@
+import { useParams } from "react-router-dom";
+import useApi from "./../../hooks/useApi";
+import { useEffect, useState } from "react";
 
-export default function CategoryDetailsPage(){
+export default function CategoryDetailsPage() {
+  const api = useApi();
+  const params = useParams();
+  const [categoryDetails, setCategoryDetails] = useState(null);
+  const [blogs, setBlogs] = useState(null);
+  const [services, setServices] = useState(null);
+
+  useEffect(() => {
+    api
+      .get(`public/categories/getBySlug/${params.slug}`)
+      .then((response) => {
+        setCategoryDetails(response.data.data.category);
+        setBlogs(response.data.data.blogs);
+        setServices(response.data.data.services);
+      })
+      .catch((err) => {
+        console.error(">>ERR", err);
+      });
+  }, []);
+
+  if (categoryDetails === null) {
     return (
+      <>
+        <h2 className="text-center">Loading...</h2>
+      </>
+    );
+  }
+  return (
     <>
-    Burasi kategori detay sayfasi
+      <h2 className="text-center">{categoryDetails.name}</h2>
+      <p>{categoryDetails.description}</p>
+
+      <br />
+      {params.slug}
     </>
-    )
+  );
 }
--- src/pages/main-page/components/category-box.jsx
+++ src/pages/main-page/components/category-box.jsx
@@ -1,21 +1,21 @@
-import styles from './category-box.module.css'
+import { Link } from "react-router-dom";
+import styles from "./category-box.module.css";
 
-
-export default function CategoryBox(props){
-
-    return(
-        <div className="card mb-4 rounded-3 shadow-sm border-primary">
-            <div className="card-header py-3 text-bg-primary border-primary">
-              <h4 className="my-0 fw-normal">{props.category.name}</h4>
-            </div>
-            <div className="card-body">
-              <img src={props.category.image} alt="" 
-               className={styles.box_image}
-              />
-              <button type="button" className="w-100 btn btn-lg btn-primary">
-                Contact us
-              </button>
-            </div>
-          </div>
-    );
+export default function CategoryBox(props) {
+  return (
+    <div className="card mb-4 rounded-3 shadow-sm border-primary">
+      <div className="card-header py-3 text-bg-primary border-primary">
+        <h4 className="my-0 fw-normal">{props.category.name}</h4>
+      </div>
+      <div className="card-body">
+        <img src={props.category.image} alt="" className={styles.box_image} />
+        <Link
+          to={"category/" + props.category.slug}
+          className="w-100 btn btn-lg btn-primary"
+        >
+          Detay
+        </Link>
+      </div>
+    </div>
+  );
 }
